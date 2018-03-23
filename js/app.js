fd5acfd30a6c3da43c8b8013a87dc40c6cbc95c4
Mohit <mohit.patil@terafinainc.com>
2018-03-22 21:55:29 -0700

    Working Code.

--- js/app.js
+++ js/app.js
@@ -8,7 +8,6 @@ myBookApp.controller('myBookCtrl', function($scope, $http) {
     $scope.noResultFound = true;
 
     $scope.getBooks = function(search) {
-
         $scope.favShow = true;
         $scope.noResultFound = true;
         $scope.searchResult = [];       // Emptied Search Array for 2nd Search.
@@ -39,55 +38,39 @@ myBookApp.controller('myBookCtrl', function($scope, $http) {
             }
         }, function myError(response) {
             $scope.noResultFound = false;
-            var errorText = response.statusText;
-            //console.log('myWelcome Bad Response', errorText);
+            //console.log('Bad Response');
         });
     };
 
     $scope.addToFav = function ($index) {
-        console.log('$index : ', $index, 'FavList', $scope.favList, 'SearchResult', $scope.searchResult);
-
+        var addToFav =$scope.searchResult[$index];
         if ($scope.favList.length === 0) {
-            //console.log('FavList 0', $scope.favList);
-             $scope.favList.push($scope.searchResult[$index]);
+            $scope.favList.push(addToFav);
         } else {
-            //console.log('FavList going');
-            /*for (var x = 0 ; x < $scope.favList.length ; x++) {
-                console.log('$scope.searchResult[$index].Title', $scope.searchResult[$index].Title);
-                console.log('$scope.favList[x].Title', $scope.favList[x].Title);
-                var n = $scope.searchResult[$index].Title.localeCompare($scope.favList[x].Title);
-                if (!n) {
-                    console.log ('Already in favourite');
-                } else {
-                    $scope.favList.push($scope.searchResult[$index]);
-                    console.log ('Adding in favourite after ');
-                }
-            }*/
-            var x = 0;
-            while(x < $scope.favList.length) {
-                if (!$scope.searchResult[$index].Title.localeCompare($scope.favList[x].Title)) {
-                    break;
-                } else {
-                    $scope.favList.push($scope.searchResult[$index]);
-                    x++;
-                }
+            if(!_.includes($scope.favList, addToFav)) {
+                console.log(_.includes($scope.favList, addToFav));
+                $scope.favList.push(addToFav);
+            } else {
+                console.log('Not Added');
             }
-        }
 
-        //$scope.favList.push($scope.searchResult[$index]);
+            /*/
+            /*var found = $scope.favList.includes($scope.searchResult[$index].Title);
+            console.log('Found', found);
+            if (!found) {
+                $scope.favList.push($scope.searchResult[$index]);
+            }*/
+        }
         console.log('$scope.favList : ', $scope.favList);
     };
 
     $scope.removeFav = function (favList, $index) {
         console.log('Remove $index : ', $index);
-        //$scope.favList.shift($scope.favList[$index]);
-
         if ($index > -1) {
             $scope.favList.splice($index, 1)
         }
         return $scope.favList;
     };
-
 
     //List and Grid View
     var elements = document.getElementsByClassName("view");
